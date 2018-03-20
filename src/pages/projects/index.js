268c030aec0e428e22c2a67f490a75a5dc5d3b80
Douglas Lilliequist <douglas.lilliequist@b-reel.com>
2018-03-20 18:03:52 +0100

    new project design done and pretty much fully responsive

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -70,28 +70,19 @@ export default class Project extends Component {
 
         })
 
-        ReactDOM.findDOMNode(this.ltrBox1)
+        // ReactDOM.findDOMNode(this.ltrBox1)
 
-        console.log(this.ltrBox1)
+        // console.log(this.ltrBox1)
 
-        this.tl.fromTo(this.ltrBox1, 0.75, {ease: Power4.easeOut, scaleY: 0.0, transformOrigin: '0% top'}, {ease: Power4.easeOut, scaleY: 0.2, transformOrigin: '0% top'})
+        this.tl.fromTo(this.ltrBox1, 0.75, {ease: Power4.easeOut, scaleY: 0.0, transformOrigin: '0% top'}, {ease: Power4.easeOut, scaleY: 1, transformOrigin: '0% top'})
         
-        this.tl.fromTo(this.ltrBox2, 0.75, {ease: Power4.easeOut, scaleY: 0.0, transformOrigin: '0% bottom'}, {ease: Power4.easeOut, scaleY: 0.2, transformOrigin: '0% bottom'}, "-= 0.75")
+        this.tl.fromTo(this.ltrBox2, 0.75, {ease: Power4.easeOut, scaleY: 0.0, transformOrigin: '0% bottom'}, {ease: Power4.easeOut, scaleY: 1, transformOrigin: '0% bottom'}, "-=0.75")
 
-        // this.tl.fromTo(this.ltrBox1, 0.5, {scaleY: 0.0, transformOrigin: '0% top'}, {scaleY: 0.2, transformOrigin: '0% top'})
-        
-        // this.tl.fromTo(this.ltrBox2, 0.5, {scaleY: 0.0, transformOrigin: '0% bottom'}, {scaleY: 0.2, transformOrigin: '0% bottom'}, "-= 0.5")
-
-        // this.tl.fromTo(this.ltrBox1, 0.5, {scaleY: 0.0, transformOrigin: '0% top'}, {scaleY: 0.2, transformOrigin: '0% top'})
-        
-        // this.tl.fromTo(this.ltrBox2, 0.5, {scaleY: 0.0, transformOrigin: '0% bottom'}, {scaleY: 0.2, transformOrigin: '0% bottom'}, "-= 0.5")
-
-
-        this.tl.staggerFromTo([this.projectNumb, this.title], 0.5, {opacity: 0.0, x: 15}, {opacity: 1.0, x: 0}, 0.15, "-=0.8")
+        this.tl.staggerFromTo([this.projectNumb, this.title, this.link], 0.5, {opacity: 0.0, x: 15}, {opacity: 1.0, x: 0}, 0.15, "-=0.8")
 
         // this.tl.staggerFromTo([this.textLine1, this.textLine2], 0.35, {strokeDashoffset: 50}, {strokeDashoffset: 0.0}, 0.1, " -= 0.15")
 
-        this.tl.staggerFromTo([this.desc, this.role, this.tech], 0.5, {opacity: 0.0, x: -15}, {opacity: 1.0, x: 0}, 0.15, "-=0.6")
+        this.tl.staggerFromTo([this.desc, this.role, this.ctx, this.tech], 0.5, {opacity: 0.0, x: -15}, {opacity: 1.0, x: 0}, 0.15, "-=0.6")
 
         // this.hideAnim = new TimelineLite({
 
@@ -104,32 +95,36 @@ export default class Project extends Component {
     }
 
     revealCopyIn() {
-
-        // this.setState({viewingProject: true, revealCopy: true}, () => {
-
-        // emitter.emit('enableExit', true)
         
         this.tl.kill()
+
+        TweenLite.killTweensOf(this.vid)
+
+        TweenLite.to(this.vid, 0.75, {
+
+            opacity: 0.4
+
+        })
         
         this.tl.play().timeScale(1)
-
-        console.log(this.ltrBox1)
-
-        // })
 
 
     }
 
     revealCopyOut() {
 
-        // this.setState({viewingProject: false, revealCopy: false}, () => {
-        // emitter.emit('enableExit', false)
-        
         this.tl.kill()
 
-        this.tl.reverse().timeScale(1.5)
+        TweenLite.killTweensOf(this.vid)
 
-        // })
+        TweenLite.to(this.vid, 0.75, {
+
+            delay: 0.5,
+            opacity: 1.0
+            
+        })
+
+        this.tl.reverse().timeScale(1.5)
 
     }
 
@@ -200,27 +195,7 @@ export default class Project extends Component {
 
     updateLayout() {
 
-        // this.textLineContainer2.style.transform = 'matrix(1, 0, 0, 1, 0,' + this.role.getBoundingClientRect().y + ')'
-        
-        // this.textLineContainer1.style.transform = 'matrix(1, 0, 0, 1, 0,' + this.desc.getBoundingClientRect().y + ')'
-
-        // this.ltrBox1.attributes.width = this.projectVid.getBoundingClientRect().width
-        
-        // this.ltrBox2.attributes.width = this.projectVid.getBoundingClientRect().width
-
-        // this.ltrBox1.attributes.height = this.projectVid.getBoundingClientRect().height * 0.2
-        
-        // this.ltrBox2.attributes.height = this.projectVid.getBoundingClientRect().height * 0.2
-
-        // this.ltrBox1.style.transform = 'translate(0%, 0%)'
-        
-        // this.ltrBox2.style.transform = 'translate(0%, 80%)'
-        // this.ltrBox2.style.transform = 'translate(0px, ' + this.projectVid.getBoundingClientRect().height * 0.8 + 'px)'
-
-        // console.log(this.ltrBox1.attributes)
-
-        // this.tl.set(this.ltrBox1, {transformOrigin: '0%, top'})
-        // this.tl.set(this.ltrBox2, {transformOrigin: '0%, bottom'})
+        // TweenLite.set(this.ltrBox2, {bottom: '0%'})
 
     }
 
@@ -232,55 +207,59 @@ export default class Project extends Component {
 
             {/* <div className = "ProjectContainer"> */}
 
-            <div className = "ProjectNumb" ref = {(numb) => {this.projectNumb = numb}}><h3>{'[ PROJECT : ' + this.projectCopy[this.props.project].number + ' ]'}</h3>
+            <div className = "ProjectNumb" ref = {(numb) => {this.projectNumb = numb}}><p>{'[ PROJECT : ' + this.projectCopy[this.props.project].number + ' ]'}</p>
             
-            {/* <svg className = "LineContainer" ref = {(container) => this.titleLineContainer = container} style = {{position: 'absolute', width: '35px', height: '2px', left: '16.25%', bottom: '3.5%'}}>
+            <svg className = "LineContainer" ref = {(container) => this.titleLineContainer = container} style = {{position: 'relative', width: '2.5vw', height: '2px'}}>
 
-                <line className = "Line" ref = {(container) => this.titleLine = container} x1 = {0} y1 = {0} x2 = {100} y2 = {0} /> 
+                <line className = "Line" ref = {(container) => this.projectLine = container} x1 = {0} y1 = {0} x2 = {100} y2 = {0} /> 
 
-            </svg> */}
+            </svg>
+
+            </div>
+
+            <div className = "ProjectLink" ref = {(el) => this.link = el}><a className = "Link" href = {this.projectCopy[this.props.project].link} target = "_blank"><p>[ VIEW PROJECT ]</p></a>
             
+                <svg className = "LineContainer" ref = {(container) => this.titleLineContainer = container} style = {{position: 'relative', width: '2.5vw', height: '2px'}}>
+
+                    <line className = "Line" ref = {(container) => this.linkLine = container} x1 = {0} y1 = {0} x2 = {100} y2 = {0} /> 
+
+                </svg>
             
             </div>
             
-                <div className = "Title" ref = {(title) => this.title = title}><h2>{this.projectCopy[this.props.project].title}</h2>
-
-                {/* <svg className = "LineContainer"> */}
-
-                    {/* <line className = "Line" ref = {(container) => this.titleLine = container} x1 = {0.0} y1 = {'7.5%'} x2 = {this.lineLength} y2 = {'7.5%'} />  */}
-
-                {/* </svg> */}
-
-                </div>
-
                 <div className = "CopyContainer" ref = {(container) => this.copyContainer = container}>
+
+                <div className = "Title" ref = {(title) => this.title = title}><h2>{this.projectCopy[this.props.project].title}</h2></div>
                 
                 <div className = "Description" ref = {(desc) => this.desc = desc}><h5 style = {{margin: 0}}>{this.projectCopy[this.props.project].description}</h5></div>
                 
-                <div className = "Role" ref = {(role) => this.role = role}><b style = {{fontStyle: 'normal'}}>role: </b>{this.projectCopy[this.props.project].role}</div>
+                <div className = "ProjectInfo" ref = {(container) => this.projectInfo = container}>
+
+                <div className = "Context" ref = {(el) => this.ctx = el}><b style = {{fontStyle: 'normal'}}>[ context: </b>{this.projectCopy[this.props.project].context}<b style = {{fontStyle: 'normal'}}> ] </b></div>
                 
-                <div className = "Tech" ref = {(tech) => this.tech = tech}><b style = {{fontStyle: 'normal'}}>tech: </b>{this.projectCopy[this.props.project].tech}</div>
+                <div className = "Role" ref = {(el) => this.role = el}><b style = {{fontStyle: 'normal'}}>[ role: </b>{this.projectCopy[this.props.project].role}<b style = {{fontStyle: 'normal'}}> ] </b></div>                
                 
+                <div className = "Tech" ref = {(el) => this.tech = el}><b style = {{fontStyle: 'normal'}}>[ tech: </b>{this.projectCopy[this.props.project].tech}<b style = {{fontStyle: 'normal'}}> ] </b></div>
+
+                </div>
+
                 </div>
 
                 <div className = "ProjectVid" ref = {(video) => this.projectVid = video}>
 
-                <svg ref = {(container) => this.letterBox = container} style = {{position: 'absolute', width: '100%', height: '100%'}}>
+                <svg ref = {(container) => this.letterBox = container} style = {{position: 'absolute', width: '100%', height: '20%', top: '0%', zIndex: '2'}}>
 
-                {/* <rect className = "LetterBox1" ref = {(el) => this.ltrBox1 = el} transformorigin = {'50% top'} width = {'100%'} height = {'100%'} style ={{position: 'absolute', fill: 'rgba(255, 255, 255, 1)'}}/> */}
-                {/* <rect className = "LetterBox2" ref = {(el) => this.ltrBox2 = el} transformorigin = {'50% bottom'} width = {'100%'} height = {'100%'} style ={{position: 'absolute', fill: 'rgba(255, 255, 255, 1)'}}/> */}
-
-                <rect className = "LetterBox1" ref = {(el) => this.ltrBox1 = el} width = {'100%'} height = {'100%'} style ={{fill: 'rgba(255, 255, 255, 1)', transform: 'translate(0%, 0%)'}}/>
-                
-                <rect className = "LetterBox2" ref = {(el) => this.ltrBox2 = el} width = {'100%'} height = {'100%'} style ={{fill: 'rgba(255, 255, 255, 1)', transform: 'translate(0%, 0%)'}}/>
-                
-                
-                {/* <rect className = "LetterBox1" ref = {(el) => this.ltrBox1 = el} style ={{position: 'absolute', width: '100%', height: '20%', fill: 'rgba(255, 255, 255, 1)', transform: 'scale(1.0, 1.0) translate(0%, 0%)'}}/>
-                <rect className = "LetterBox2" ref = {(el) => this.ltrBox2 = el} style ={{position: 'absolute', width: '100%', height: '20%', fill: 'rgba(255, 255, 255, 1)', transform: 'translate(0%, 80%)'}}/> */}
+                <rect className = "LetterBox1" ref = {(el) => this.ltrBox1 = el} width = {'100%'} height = {'100%'} style ={{position: 'absolute', fill: 'rgba(255, 255, 255, 1)'}}/>
 
                 </svg>
 
-                    <video autoPlay = {true} loop = {true} crossOrigin = {'Anonymous'}>
+                <svg ref ={(container) => this.letterBox2 = container} style = {{position: 'absolute', width: '100%', height: '20%', bottom: '0%', zIndex: '2'}}>
+
+                    <rect className = "LetterBox2" ref = {(el) => this.ltrBox2 = el} width = {'100%'} height = {'100%'} style ={{position: 'absolute', fill: 'rgba(255, 255, 255, 1)'}}/>
+
+                </svg>
+
+                    <video ref = {(vid) => this.vid = vid} autoPlay = {true} loop = {true} crossOrigin = {'Anonymous'}>
 
                         <source src = {this.projectCopy[this.props.project].vid} type = {'video/mp4'} />
 
--- src/pages/projects/projectCopy.js
+++ src/pages/projects/projectCopy.js
@@ -3,11 +3,12 @@ export const projectCopy = {
     doli: {
         
         number: '004',
-        title: '[ DOLI ]',
-        description: 'Personal, ongoing project that is meant to be an expressive and virtual representation of myself.',
+        title: 'DOLI',
+        description: 'Interactive GPGPU particles',
         tech: 'WebGL(THREE.js) / instancing / GPGPU simulation',
         role: 'developer (Front-End), concept creator, designer',
         context: 'Personal Project',
+        link: 'https://doli91.github.io/doliv2/',
         vid: './src/media/doli.mp4'
 
     },
@@ -15,11 +16,12 @@ export const projectCopy = {
     redCrossWebVR: {
         
         number: '003',
-        title: '[ We\'re All Human ]',
-        description: 'WebVR project I built in collaboration with Swiss agency TBNT for a client.',
+        title: 'We\'re All Human',
+        description: 'WebVR experience built in collaboration with TBNT',
         tech: 'WebVR / WebGL(THREE.js) / GLSL',
         role: 'developer (Front-End)',
         context: 'Client Project',
+        link: 'https://30ans.redcrossmuseum.ch/vr/',
         vid: './src/media/tbntproj.mp4'
 
     },
@@ -27,14 +29,14 @@ export const projectCopy = {
     internshipProject: {
 
         number: '002',
-        title: '[ Intership Project ]',
+        title: 'Intership Project',
         // description: 'An RnD project I collaborated on during my internship at B-Reel London, which uses detection data from YOLO to affect the content a WebGL experience in real time. ' +
         // 'The project uses custom software that I built with OpenFrameworks, which sends socket messages containing the detection data and screen space coordinates which was used for my teammember\'s tracking solution. With this, I was then able to animate lines that follows the detections.',
-        description: 'An RnD project I collaborated on during my internship at B-Reel London, which uses detection data from YOLO to affect the content a WebGL experience in real time.',
-        // 'The project uses custom software that I built with OpenFrameworks, which sends socket messages containing the detection data and screen space coordinates which was used for my teammember\'s tracking solution.',
+        description: 'Project I collaborated on during my internship at B-Reel London',
         tech: 'WebGL(THREE.Js) / OpenFrameworks / YOLO / Node.js / sockets.io',
         role: 'developer(OpenFrameWorks, assisting Front-End)',
         context: 'RnD Project',
+        link: 'no_link_available',
         vid: './src/media/intrnshpproj.mp4'
 
     },
@@ -42,11 +44,12 @@ export const projectCopy = {
     hyperInstallation: {
 
         number: '001',
-        title: '[ Good Night Sweet Heart ]',
-        description: 'Team project for the Exploring Tech 2015 event held by Hyper Island. The installation is an art puzzle game, were the user unravels a mysterious disappearance with help of memories associated with the items in the room.',
+        title: 'Good Night Sweet Heart',
+        description: 'Art Puzzle Installation',
         tech: 'Arduino / Processing / Resolume',
         role: 'developer, designer',
-        context: 'School Project',
+        context: 'Team Project, Hyper Island',
+        link: 'no_link_available',
         vid: './src/media/gdnghtswthrtweb.mp4'
 
     }
