dfdddab0f5137504efd8e20b1171dfa9c9c59d5e
Douglas Lilliequist <douglas.lilliequist@b-reel.com>
2018-12-05 22:04:42 +0100

    new project and removing redcross

--- src/pages/projects/projectContent.js
+++ src/pages/projects/projectContent.js
@@ -1,12 +1,27 @@
 export const projectContent = {
 
-    doli: {
+    needaHand: {
         
         number: '001',
+        title: 'Need A Hand?',
+        description: 'Interactive, displacing mesh',
+        tech: 'WebGL(THREE.js) / Computed triangles from mesh data',
+        role: 'developer, concept creator',
+        context: 'Experiment',
+        link: 'https://doli91.github.io/needAHand/',
+        vid: './src/media/needahand.mp4',
+        cursorColor: 'dark',
+        note: ''
+
+    },
+
+    doli: {
+        
+        number: '002',
         title: 'DOLI',
         description: 'Interactive GPGPU powered instances',
         tech: 'WebGL(THREE.js) / instancing / GPGPU simulation',
-        role: 'developer (Front-End), concept creator, designer',
+        role: 'developer, concept creator, designer',
         context: 'Personal Project',
         link: 'https://doli91.github.io/doliv2/',
         vid: './src/media/doli.mp4',
@@ -17,7 +32,7 @@ export const projectContent = {
 
     internshipProject: {
 
-        number: '002',
+        number: '003',
         title: 'YOLO + WebGL',
         description: 'RealTime animations based on detection data from YOLO',
         tech: 'WebGL(THREE.js) / OpenFrameworks / YOLO / Node.js / sockets.io',
@@ -32,7 +47,7 @@ export const projectContent = {
 
     hyperInstallation: {
 
-        number: '003',
+        number: '004',
         title: 'Good Night Sweet Heart',
         description: 'Art Puzzle Installation',
         tech: 'Arduino / Processing / Resolume',
@@ -44,20 +59,5 @@ export const projectContent = {
         note: ''
 
     },
-
-    redCrossWebVR: {
-        
-        number: '004',
-        title: 'We\'re All Human',
-        description: 'WebVR experience built in collaboration with TBNT',
-        tech: 'WebVR / WebGL(THREE.js) / GLSL',
-        role: 'developer (Front-End)',
-        context: 'Client Project',
-        link: 'https://30ans.redcrossmuseum.ch/vr/',
-        vid: './src/media/tbntprojv2.mp4',
-        cursorColor: 'dark',
-        note: ' '
-
-    }
 
 }
